17f2057e4cd258600761124b6e1cc1bc846e1762
Kyler Kleibrink <kkleibrink@outlook.com>
2023-04-27 12:48:58 -0600

    Landing

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -3,53 +3,38 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const Landing = ({ auth, router }) => {
+const Landing = ({ auth }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
     if (auth.isAuthenticated) {
-    navigate("/dashboard");
+      navigate("/dashboard");
     }
-    }, [auth.isAuthenticated, router]);
+  }, [auth.isAuthenticated, navigate]);
 
   return (
-    <div style={{ height: "75vh" }} className="container valign-wrapper">
-      <div className="row">
-        <div className="col s12 center-align">
-          <h4>
-            <b>Track</b> user issues with style.{" "}
-            <span style={{ fontFamily: "monospace" }}>CNRL</span> Issue Tracker
-          </h4>
-          <p className="flow-text grey-text text-darken-1">
-            Login to see the magic happen
-          </p>
-          <br />
-          <div className="col s6">
-            <Link
-              to="/register"
-              style={{
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-              }}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Register
-            </Link>
-          </div>
-          <div className="col s6">
-            <Link
-              to="/login"
-              style={{
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-              }}
-              className="btn btn-large btn-flat waves-effect white black-text"
-            >
-              Log In
-            </Link>
-          </div>
+    <div className="h-[75vh] flex items-center justify-center">
+      <div className="text-center">
+        <h4 className="font-bold">
+          <span className="text-black">Track</span> user issues with style.{" "}
+          <span className="font-mono"></span> Issue Tracker
+        </h4>
+        <p className="text-gray-800 text-lg">
+          Login to see the magic happen
+        </p>
+        <div className="mt-4 space-x-4">
+          <Link
+            to="/register"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded"
+          >
+            Register
+          </Link>
+          <Link
+            to="/login"
+            className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-6 rounded"
+          >
+            Log In
+          </Link>
         </div>
       </div>
     </div>
